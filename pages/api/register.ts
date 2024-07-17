d5cb65e7cdaceddffbf2ff44ba58ad0ba302d2e8
Lean <lean@leans-mbp.home>
2024-07-16 23:01:16 -0400

    initial commit

---
+++ pages/api/register.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../lib/supabase";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method !== "POST") {
+		return res.status(405).json({ error: "Method not allowed" });
+	}
+
+	const { email, password, role } = req.body;
+
+	const { user, error } = await supabase.auth.signUp({ email, password });
+
+	if (error) {
+		return res.status(500).json({ error: error.message });
+	}
+
+	const { data, roleError } = await supabase
+		.from("user_roles")
+		.insert([{ user_id: user!.id, role }]);
+
+	if (roleError) {
+		return res.status(500).json({ error: roleError.message });
+	}
+
+	res.status(200).json({ message: "User registered succesfully", user });
+}
